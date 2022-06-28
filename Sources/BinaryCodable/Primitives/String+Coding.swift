6c7fb6ec14fcf8e938e55ee88e910ea0025e9abc
Christoph Hagen <christoph@spacemasters.eu>
2022-06-28 23:58:14 +0200

    Rename files

--- Sources/BinaryCodable/Primitives/String+Coding.swift
+++ Sources/BinaryCodable/Primitives/String+Coding.swift
@@ -24,15 +24,6 @@ extension String: DecodablePrimitive {
     }
 }
 
-extension String {
-    
-    func indented(by indentation: String = "  ") -> String {
-        components(separatedBy: "\n")
-            .map { indentation + $0 }
-            .joined(separator: "\n")
-    }
-}
-
 extension String: ProtobufCodable {
     
 }
