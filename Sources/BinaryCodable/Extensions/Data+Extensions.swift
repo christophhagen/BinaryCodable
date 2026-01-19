554ee2bef13b2e5f2168f8d6c7f6f862ee6d03b7
Christoph Hagen <github@christophhagen.de>
2026-01-19 22:14:14 +0100

    Test fix for swift 6 crash

--- Sources/BinaryCodable/Extensions/Data+Extensions.swift
+++ Sources/BinaryCodable/Extensions/Data+Extensions.swift
@@ -37,9 +37,16 @@ extension Data {
      - Parameter type: The type to interpret
      */
     func interpreted<T>(as type: T.Type = T.self) -> T {
-        Data(self).withUnsafeBytes {
-            $0.baseAddress!.load(as: T.self)
+#if swift(>=6.0)
+        withUnsafeBytes { rawBuffer in
+            rawBuffer.loadUnaligned(as: T.self)
         }
+#else
+        precondition(count >= MemoryLayout<T>.size)
+        return withUnsafeBytes { rawBuffer in
+            rawBuffer.baseAddress!.assumingMemoryBound(to: T.self).pointee
+        }
+#endif
     }
 
     /**
